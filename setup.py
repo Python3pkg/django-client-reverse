b57da8d095c35b74766e96223c995bc3c3e0d625
Jake Howard <git@theorangeone.net>
2015-12-10 13:25:59 +0000

    Added details and re-jig setup.py

--- setup.py
+++ setup.py
@@ -1,79 +1,22 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from distutils.core import setup
+from json import load as json_load
+LONG_DESCRIPTION = None
+try:
+    LONG_DESCRIPTION = open('README.md').read()
+except:
+    pass
 
-from __future__ import print_function
-from setuptools import setup
-import re
-import os
-import sys
-
-
-name = 'django-client-reverse'
-package = 'django_client_reverse'
-description = 'Django module to access URL reversing client-side'
-url = 'https://github.com/RealOrangeOne/django-client-reverse'
-author = 'TheOrangeOne'
-author_email = 'git@theorangeone.net'
-license = 'BSD'
-install_requires = [
-    "Django>=1.8",
-    "djangorestframework>=3.3.0",
-]
-
-long_description = description
-
-def get_version(package):
-    """
-    Return package version as listed in `__version__` in `init.py`.
-    """
-    init_py = open(os.path.join(package, '__init__.py')).read()
-    return re.search("^__version__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(1)
-
-
-def get_packages(package):
-    """
-    Return root package and all sub-packages.
-    """
-    return [dirpath
-            for dirpath, dirnames, filenames in os.walk(package)
-            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
-
-
-def get_package_data(package):
-    """
-    Return all files under the root package, that are not in a
-    package themselves.
-    """
-    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
-            for dirpath, dirnames, filenames in os.walk(package)
-            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
-
-    filepaths = []
-    for base, filenames in walk:
-        filepaths.extend([os.path.join(base, filename)
-                          for filename in filenames])
-    return {package: filepaths}
-
-
-if sys.argv[-1] == 'publish':
-    os.system("python setup.py sdist upload")
-    args = {'version': get_version(package)}
-    print("You probably want to also tag the version now:")
-    print("  git tag -a %(version)s -m 'version %(version)s'" % args)
-    print("  git push --tags")
-    sys.exit()
-
-
+DETAILS = None
+with open('details.json') as file:
+    DETAILS = json_load(file)
 setup(
-    name=name,
-    version=get_version(package),
-    url=url,
-    license=license,
-    description=description,
-    long_description=long_description,
-    author=author,
-    author_email=author_email,
-    packages=get_packages(package),
-    package_data=get_package_data(package),
-    install_requires=install_requires,
-)
+    name = DETAILS['name'],
+    packages = DETAILS['packages'],
+    version = DETAILS['version'],
+    description = DETAILS['description'],
+    long_description = LONG_DESCRIPTION,
+    author = DETAILS['author'],
+    author_email = DETAILS['author_email'],
+    url = DETAILS['url'],
+    license = DETAILS['license'],
+    platforms = DETAILS['platforms']
